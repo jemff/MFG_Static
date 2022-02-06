2e3e849149e926b07fc00f36e5da1c2471699ac0
jemff <emilfriisfrolich@gmail.com>
2022-02-06 15:34:24 +0100

    Fixed utf8 error

--- phase_plot_static.py
+++ phase_plot_static.py
@@ -3,82 +3,70 @@ import matplotlib.pyplot as plt
 from infrastructure import *
 from scipy.integrate import solve_ivp
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('font', size = 10)
-# These are the "Tableau 20" colors as RGB.
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+def stat_pp():
+    layers = 2
+    segments = 60
+    length = 1
+    tot_points = layers*segments
+    Mx = simple_method(length, tot_points)
 
-for i in range(len(tableau20)):
-    r, g, b = tableau20[i]
-    tableau20[i] = (r / 255., g / 255., b / 255.)
+    pop_max = 1.5
+    pop_min = 0
+    car_cap = 40
+    fidelity = 15
+    ppop_max = 0.6
+    pop_varc = np.linspace(0, pop_max, fidelity)
+    pop_varp = np.linspace(0, ppop_max, fidelity)
 
-layers = 2
-segments = 60
-length = 1
-tot_points = layers*segments
-Mx = simple_method(length, tot_points)
+    min_pop = 0
+    out = twod_predator_prey_dyn(Mx = Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap)
+    inte = np.ones(tot_points).reshape(1,tot_points)
+    par = {'res_renew': 1, 'eff': 0.1, 'c_handle': 1, 'c_enc_freq': 1, 'c_met_loss': 0.001, 'p_handle': 0.01,
+           'p_enc_freq': 0.1, 'p_met_loss': 0.15, 'competition': 0, 'q': 3}
 
-pop_max = 1.5
-pop_min = 0
-car_cap = 40
-fidelity = 15
-ppop_max = 0.6
-pop_varc = np.linspace(0, pop_max, fidelity)
-pop_varp = np.linspace(0, ppop_max, fidelity)
+    beta = np.exp(-(par['q'] * Mx.x) ** 2)  # +0.001 2*np.exp(-Mx.x)/(1+np.exp(-Mx.x))#np.exp(-Mx.x**2)#+0.001
+    beta = 0.5 * 1 / (inte @ (Mx.M @ beta)) * beta + 0.0001
 
-min_pop = 0
-out = twod_predator_prey_dyn(Mx = Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap)
-inte = np.ones(tot_points).reshape(1,tot_points)
-par = {'res_renew': 1, 'eff': 0.1, 'c_handle': 1, 'c_enc_freq': 1, 'c_met_loss': 0.001, 'p_handle': 0.01,
-       'p_enc_freq': 0.1, 'p_met_loss': 0.15, 'competition': 0, 'q': 3}
+    res_conc = np.exp(-par['q'] * Mx.x)  # np.exp(-Mx.x**2)#np.exp(-Mx.x)+0.001
+    res_conc = 1 / (inte @ (Mx.M @ res_conc)) * res_conc + 0.0001
+    #, beta_f=lambda x: np.exp(-(par['q'] * x) ** 2) + 0.001,
+    #                                     res_conc_f=lambda x: np.exp(-par['q'] * x)
+    gridx, gridy= np.meshgrid(pop_varc, pop_varp)
+    vectors = np.zeros((fidelity, fidelity, 2))
+    for i in range(fidelity):
+        for j in range(fidelity):
+            pop_ij = np.array([pop_varc[i], pop_varp[j]])
+            sigma = np.ones(segments*layers)
+            sigma_p = np.ones(segments*layers)
+            cons_dyn = inte @ (Mx.M @ (sigma * pop_ij[0] * (
+                        1 - pop_ij[0] * sigma / (res_conc * car_cap)))) - inte @ (
+                                   Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
+                                   par['p_enc_freq'] + par['p_handle'] * inte @ (
+                                       Mx.M @ (pop_ij[0] * sigma * beta * sigma_p)))
+            pred_dyn = par['eff'] * inte @ (Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
+                        par['p_enc_freq'] + par['p_handle'] * inte @ (Mx.M @ (pop_ij[0] * sigma * beta * sigma_p))) - par[
+                           'p_met_loss'] * pop_ij[1] - par['competition'] * inte @ (Mx.M @ (sigma_p ** 2 * beta)) * \
+                       pop_ij[1]**2
 
-beta = np.exp(-(par['q'] * Mx.x) ** 2)  # +0.001 2*np.exp(-Mx.x)/(1+np.exp(-Mx.x))#np.exp(-Mx.x**2)#+0.001
-beta = 0.5 * 1 / (inte @ (Mx.M @ beta)) * beta + 0.0001
+            vectors[i,j,0] = cons_dyn
+            vectors[i,j,1] = pred_dyn
+            N = np.linalg.norm(vectors[i, j, :])
+            vectors[i, j,:] = vectors[i, j,:]/N
 
-res_conc = np.exp(-par['q'] * Mx.x)  # np.exp(-Mx.x**2)#np.exp(-Mx.x)+0.001
-res_conc = 1 / (inte @ (Mx.M @ res_conc)) * res_conc + 0.0001
-#, beta_f=lambda x: np.exp(-(par['q'] * x) ** 2) + 0.001,
-#                                     res_conc_f=lambda x: np.exp(-par['q'] * x)
-gridx, gridy= np.meshgrid(pop_varc, pop_varp)
-vectors = np.zeros((fidelity, fidelity, 2))
-for i in range(fidelity):
-    for j in range(fidelity):
-        pop_ij = np.array([pop_varc[i], pop_varp[j]])
-        sigma = np.ones(segments*layers)
-        sigma_p = np.ones(segments*layers)
-        cons_dyn = inte @ (Mx.M @ (sigma * pop_ij[0] * (
-                    1 - pop_ij[0] * sigma / (res_conc * car_cap)))) - inte @ (
-                               Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
-                               par['p_enc_freq'] + par['p_handle'] * inte @ (
-                                   Mx.M @ (pop_ij[0] * sigma * beta * sigma_p)))
-        pred_dyn = par['eff'] * inte @ (Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
-                    par['p_enc_freq'] + par['p_handle'] * inte @ (Mx.M @ (pop_ij[0] * sigma * beta * sigma_p))) - par[
-                       'p_met_loss'] * pop_ij[1] - par['competition'] * inte @ (Mx.M @ (sigma_p ** 2 * beta)) * \
-                   pop_ij[1]**2
-
-        vectors[i,j,0] = cons_dyn
-        vectors[i,j,1] = pred_dyn
-        N = np.linalg.norm(vectors[i, j, :])
-        vectors[i, j,:] = vectors[i, j,:]/N
-
-fig, ax = plt.subplots(figsize=(6/2.54, 6/2.54))
-q = ax.quiver(gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, scale=50, headwidth=1, color=tableau20[14])
-ax.set_xlabel("Biomass, (C)")
-ax.set_ylabel("Biomass, (P)")
+    #fig, ax = plt.subplots(figsize=(6/2.54, 6/2.54))
+    #q = ax.quiver(gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, scale=50, headwidth=1, color=tableau20[14])
+    #ax.set_xlabel("Biomass, (C)")
+    #ax.set_ylabel("Biomass, (P)")
 
 
-t_begin = 0
-t_end = 20
-t_eval = np.linspace(t_begin, t_end, 50)
-dyn_data_1 = solve_ivp(lambda t, y: dynamics_static(t, y, par = par,Mx = Mx, inte = inte, car_cap=car_cap), t_span=[t_begin, t_end], y0 = np.array([1.5/2, 1/3]), dense_output=True, t_eval = t_eval)
+    t_begin = 0
+    t_end = 20
+    t_eval = np.linspace(t_begin, t_end, 50)
+    dyn_data_1 = solve_ivp(lambda t, y: dynamics_static(t, y, par = par,Mx = Mx, inte = inte, car_cap=car_cap), t_span=[t_begin, t_end], y0 = np.array([1.5/2, 1/3]), dense_output=True, t_eval = t_eval)
 
-ax.plot(dyn_data_1.y[0,:], dyn_data_1.y[1,:], color=tableau20[8])
+    return gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, dyn_data_1
+#ax.plot(dyn_data_1.y[0,:], dyn_data_1.y[1,:], color=tableau20[8])
 
-plt.savefig('results/plots/dynamics_static.pdf')
+#plt.savefig('results/plots/dynamics_static.pdf')
 
 
--- phase_plots.py
+++ phase_plots.py
@@ -2,97 +2,82 @@ from predator_prey_sys import twod_predator_prey_dyn, dynamics
 import matplotlib.pyplot as plt
 from infrastructure import *
 from scipy.integrate import solve_ivp
+def dyn_pp():
+    layers = 1
+    segments = 30
+    length = 1
+    tot_points = layers*segments
+    Mx = simple_method(length, tot_points)
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('font', size = 10)
+    pop_max = 10
+    pop_min = 0
+    car_cap = 40
+    fidelity = 15
+    ppop_max = 10
+    pop_varc = np.linspace(0, pop_max, fidelity)
+    pop_varp = np.linspace(0, ppop_max, fidelity)
 
+    min_pop = 0
+    out = twod_predator_prey_dyn(Mx = Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap)
+    inte = np.ones(tot_points).reshape(1,tot_points)
+    par = {'res_renew': 1, 'eff': 0.1, 'c_handle': 1, 'c_enc_freq': 1, 'c_met_loss': 0.001, 'p_handle': 0.01,
+           'p_enc_freq': 0.1, 'p_met_loss': 0.15, 'competition': 0, 'q': 3}
 
-# These are the "Tableau 20" colors as RGB.
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+    beta = np.exp(-(par['q'] * Mx.x) ** 2)  # +0.001 2*np.exp(-Mx.x)/(1+np.exp(-Mx.x))#np.exp(-Mx.x**2)#+0.001
+    beta = 0.5 * 1 / (inte @ (Mx.M @ beta)) * beta + 0.0001
 
-for i in range(len(tableau20)):
-    r, g, b = tableau20[i]
-    tableau20[i] = (r / 255., g / 255., b / 255.)
+    res_conc = np.exp(-par['q'] * Mx.x)  # np.exp(-Mx.x**2)#np.exp(-Mx.x)+0.001
+    res_conc = 1 / (inte @ (Mx.M @ res_conc)) * res_conc + 0.0001
+    #, beta_f=lambda x: np.exp(-(par['q'] * x) ** 2) + 0.001,
+    #                                     res_conc_f=lambda x: np.exp(-par['q'] * x)
+    gridx, gridy= np.meshgrid(pop_varc, pop_varp)
+    vectors = np.zeros((fidelity, fidelity, 2))
+    for i in range(fidelity):
+        for j in range(fidelity):
+            pop_ij = np.array([pop_varc[i], pop_varp[j]])
+            out = twod_predator_prey_dyn(pops=pop_ij, Mx=Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap,
+                                         minimal_pops=0, par=par, warmstart_info=out)
+            sigma = out['x0'][0: tot_points]
+            sigma_p = out['x0'][tot_points: 2*tot_points]
+            cons_dyn = inte @ (Mx.M @ (sigma * pop_ij[0] * (
+                        1 - pop_ij[0] * sigma / (res_conc * car_cap)))) - inte @ (
+                                   Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
+                                   par['p_enc_freq'] + par['p_handle'] * inte @ (
+                                       Mx.M @ (pop_ij[0] * sigma * beta * sigma_p)))
+            pred_dyn = par['eff'] * inte @ (Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
+                        par['p_enc_freq'] + par['p_handle'] * inte @ (Mx.M @ (pop_ij[0] * sigma * beta * sigma_p))) - par[
+                           'p_met_loss'] * pop_ij[1] - par['competition'] * inte @ (Mx.M @ (sigma_p ** 2 * beta)) * \
+                       pop_ij[1]**2
 
-layers = 1
-segments = 30
-length = 1
-tot_points = layers*segments
-Mx = simple_method(length, tot_points)
+            vectors[i,j,0] = cons_dyn
+            vectors[i,j,1] = pred_dyn
+            N = np.linalg.norm(vectors[i, j, :])
+            vectors[i, j,:] = vectors[i, j,:]/N
 
-pop_max = 10
-pop_min = 0
-car_cap = 40
-fidelity = 15
-ppop_max = 10
-pop_varc = np.linspace(0, pop_max, fidelity)
-pop_varp = np.linspace(0, ppop_max, fidelity)
+    #fig, ax = plt.subplots(figsize=(6/2.54, 6/2.54))
+    #q = ax.quiver(gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, scale=50, headwidth=1, color=tableau20[14])
+    #ax.set_xlabel("Biomass, (C)")
+    #ax.set_ylabel("Biomass, (P)")
 
-min_pop = 0
-out = twod_predator_prey_dyn(Mx = Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap)
-inte = np.ones(tot_points).reshape(1,tot_points)
-par = {'res_renew': 1, 'eff': 0.1, 'c_handle': 1, 'c_enc_freq': 1, 'c_met_loss': 0.001, 'p_handle': 0.01,
-       'p_enc_freq': 0.1, 'p_met_loss': 0.15, 'competition': 0, 'q': 3}
+    step_size = 0.1
+    num_points = 300
+    dyn_data = np.zeros((num_points, 2))
+    y0 = np.array([0.8*pop_max, 0.8*ppop_max])
+    dyn_data[0] = y0
+    out = twod_predator_prey_dyn(Mx = Mx, pops=dyn_data[i], fixed_point=False, warmstart_out=True, car_cap=car_cap)
 
-beta = np.exp(-(par['q'] * Mx.x) ** 2)  # +0.001 2*np.exp(-Mx.x)/(1+np.exp(-Mx.x))#np.exp(-Mx.x**2)#+0.001
-beta = 0.5 * 1 / (inte @ (Mx.M @ beta)) * beta + 0.0001
-
-res_conc = np.exp(-par['q'] * Mx.x)  # np.exp(-Mx.x**2)#np.exp(-Mx.x)+0.001
-res_conc = 1 / (inte @ (Mx.M @ res_conc)) * res_conc + 0.0001
-#, beta_f=lambda x: np.exp(-(par['q'] * x) ** 2) + 0.001,
-#                                     res_conc_f=lambda x: np.exp(-par['q'] * x)
-gridx, gridy= np.meshgrid(pop_varc, pop_varp)
-vectors = np.zeros((fidelity, fidelity, 2))
-for i in range(fidelity):
-    for j in range(fidelity):
-        pop_ij = np.array([pop_varc[i], pop_varp[j]])
-        out = twod_predator_prey_dyn(pops=pop_ij, Mx=Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap,
+    for i in range(1,num_points):
+        out = twod_predator_prey_dyn(pops=dyn_data[i-1], Mx=Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap,
                                      minimal_pops=0, par=par, warmstart_info=out)
         sigma = out['x0'][0: tot_points]
-        sigma_p = out['x0'][tot_points: 2*tot_points]
-        cons_dyn = inte @ (Mx.M @ (sigma * pop_ij[0] * (
-                    1 - pop_ij[0] * sigma / (res_conc * car_cap)))) - inte @ (
-                               Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
-                               par['p_enc_freq'] + par['p_handle'] * inte @ (
-                                   Mx.M @ (pop_ij[0] * sigma * beta * sigma_p)))
-        pred_dyn = par['eff'] * inte @ (Mx.M @ (pop_ij[0] * pop_ij[1] * sigma * beta * sigma_p)) / (
-                    par['p_enc_freq'] + par['p_handle'] * inte @ (Mx.M @ (pop_ij[0] * sigma * beta * sigma_p))) - par[
-                       'p_met_loss'] * pop_ij[1] - par['competition'] * inte @ (Mx.M @ (sigma_p ** 2 * beta)) * \
-                   pop_ij[1]**2
-
-        vectors[i,j,0] = cons_dyn
-        vectors[i,j,1] = pred_dyn
-        N = np.linalg.norm(vectors[i, j, :])
-        vectors[i, j,:] = vectors[i, j,:]/N
-
-fig, ax = plt.subplots(figsize=(6/2.54, 6/2.54))
-q = ax.quiver(gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, scale=50, headwidth=1, color=tableau20[14])
-ax.set_xlabel("Biomass, (C)")
-ax.set_ylabel("Biomass, (P)")
-
-step_size = 0.1
-num_points = 300
-dyn_data = np.zeros((num_points, 2))
-y0 = np.array([0.8*pop_max, 0.8*ppop_max])
-dyn_data[0] = y0
-out = twod_predator_prey_dyn(Mx = Mx, pops=dyn_data[i], fixed_point=False, warmstart_out=True, car_cap=car_cap)
-
-for i in range(1,num_points):
-    out = twod_predator_prey_dyn(pops=dyn_data[i-1], Mx=Mx, fixed_point=False, warmstart_out=True, car_cap=car_cap,
-                                 minimal_pops=0, par=par, warmstart_info=out)
-    sigma = out['x0'][0: tot_points]
-    sigma_p = out['x0'][tot_points: 2 * tot_points]
-    dyns = dynamics(dyn_data[i-1], par = par,Mx = Mx, inte = inte, car_cap=car_cap, sigma = sigma, sigma_p = sigma_p)
-    dyn_data[i] = dyn_data[i-1] + step_size*dyns
+        sigma_p = out['x0'][tot_points: 2 * tot_points]
+        dyns = dynamics(dyn_data[i-1], par = par,Mx = Mx, inte = inte, car_cap=car_cap, sigma = sigma, sigma_p = sigma_p)
+        dyn_data[i] = dyn_data[i-1] + step_size*dyns
 
 
-ax.plot(dyn_data[:,0], dyn_data[:,1], color=tableau20[0])
-plt.savefig('results/plots/dynamics.pdf')
+    return gridx, gridy, vectors[:,:,0].T, vectors[:,:,1].T, dyn_data
+#ax.plot(dyn_data[:,0], dyn_data[:,1], color=tableau20[0])
+#plt.savefig('results/plots/dynamics.pdf')
 
-plt.show()
+#plt.show()
 
